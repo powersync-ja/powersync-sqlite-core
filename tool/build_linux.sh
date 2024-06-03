ac6247db000229128cbe6926b711010850414b6e
Mugi Khan <mugikhan@gmail.com>
2024-06-03 14:56:11 +0200

    Test building linux

--- tool/build_linux.sh
+++ tool/build_linux.sh
@@ -1,9 +1,12 @@
 if [ "$1" = "x64" ]; then
+  rustup toolchain install nightly-2024-05-18-x86_64-unknown-linux-gnu
+  rustup component add rust-src --toolchain nightly-2024-05-18-x86_64-unknown-linux-gnu
   rustup target add target x86_64-unknown-linux-gnu
   cargo build -p powersync_loadable --release
   mv "target/release/libpowersync.so" "libpowersync_x64.so"
 else
-  rustup target remove x86_64-unknown-linux-gnu
+  rustup toolchain install nightly-2024-05-18-aarch64-unknown-linux-gnu
+  rustup component add rust-src --toolchain nightly-2024-05-18-aarch64-unknown-linux-gnu
   rustup target add aarch64-unknown-linux-gnu
   cargo build -p powersync_loadable --release
   mv "target/release/libpowersync.so" "libpowersync_aarch64.so"
--- tool/build_windows.sh
+++ tool/build_windows.sh
@@ -1,9 +1,12 @@
 if [ "$1" = "x64" ]; then
+  rustup toolchain install nightly-2024-05-18-x86_64-pc-windows-msvc
+  rustup component add rust-src --toolchain nightly-2024-05-18-x86_64-pc-windows-msvc
   rustup target add x86_64-pc-windows-msvc
   cargo build -p powersync_loadable --release
   mv "target/release/powersync.dll" "powersync_x64.dll"
 else
-  rustup target remove x86_64-pc-windows-msvc
+  rustup toolchain install nightly-2024-05-18-aarch64-pc-windows-msvc
+  rustup component add rust-src --toolchain nightly-2024-05-18-aarch64-pc-windows-msvc
   rustup target add aarch64-pc-windows-msvc
   cargo build -p powersync_loadable --release
   mv "target/release/powersync.dll" "powersync_aarch64.dll"
