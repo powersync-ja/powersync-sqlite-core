d6bab3c789dc1dfe85ca8d4e9ab6c6043e2303e2
Mughees Khan <mugikhan@gmail.com>
2024-06-12 15:07:22 +0200

    Lower minSDK version for android (#9)
    * Lower android minSDK version to 21
    
    * Fix android build setup
    
    * Bump version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "co.powersync"
-version = "0.1.6"
+version = "0.1.7"
 description = "PowerSync Core SQLite Extension"
 
 repositories {
@@ -44,7 +44,7 @@ android {
     namespace = "co.powersync.sqlitecore"
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 21
     }
 
     sourceSets {
