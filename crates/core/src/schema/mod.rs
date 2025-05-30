e3f03fd3d90e6424c91a50158a1b31b15fa7f502
Simon Binder <simon@journeyapps.com>
2025-05-30 14:00:16 +0200

    Use serde to read table and index schema

--- crates/core/src/schema/mod.rs
+++ crates/core/src/schema/mod.rs
@@ -1,11 +1,16 @@
 mod management;
 mod table_info;
 
+use alloc::vec::Vec;
+use serde::Deserialize;
 use sqlite::ResultCode;
 use sqlite_nostd as sqlite;
-pub use table_info::{
-    ColumnInfo, ColumnNameAndTypeStatement, DiffIncludeOld, TableInfo, TableInfoFlags,
-};
+pub use table_info::{Column, DiffIncludeOld, Table, TableInfoFlags};
+
+#[derive(Deserialize)]
+pub struct Schema {
+    tables: Vec<table_info::Table>,
+}
 
 pub fn register(db: *mut sqlite::sqlite3) -> Result<(), ResultCode> {
     management::register(db)
