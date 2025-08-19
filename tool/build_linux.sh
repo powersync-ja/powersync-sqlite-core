9373c076e60e416940c11bc4f472ead6b294564a
Simon Binder <simon@journeyapps.com>
2025-08-19 10:11:19 +0200

    Refactor CI build (#121)
    This refactors the CI build with the goal of making non-released branches easier to test and to re-use build logic between the test and the release workflow.

--- tool/build_linux.sh
+++ tool/build_linux.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "x64" ]; then
-  cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target x86_64-unknown-linux-gnu
-  mv "target/x86_64-unknown-linux-gnu/release/libpowersync.so" "libpowersync_x64.so"
-else
-  cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target aarch64-unknown-linux-gnu
-  mv "target/aarch64-unknown-linux-gnu/release/libpowersync.so" "libpowersync_aarch64.so"
-fi
+case "$1" in
+  x64)
+    cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target x86_64-unknown-linux-gnu
+    mv "target/x86_64-unknown-linux-gnu/release/libpowersync.so" "libpowersync_x64.so"
+    ;;
+  x86)
+    cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target i686-unknown-linux-gnu
+    mv "target/i686-unknown-linux-gnu/release/libpowersync.so" "libpowersync_x86.so"
+    ;;
+  aarch64)
+    cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target aarch64-unknown-linux-gnu
+    mv "target/aarch64-unknown-linux-gnu/release/libpowersync.so" "libpowersync_aarch64.so"
+    ;;
+  armv7)
+    cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target armv7-unknown-linux-gnueabihf
+    mv "target/armv7-unknown-linux-gnueabihf/release/libpowersync.so" "libpowersync_armv7.so"
+    ;;
+  riscv64gc)
+    cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target riscv64gc-unknown-linux-gnu
+    mv "target/riscv64gc-unknown-linux-gnu/release/libpowersync.so" "libpowersync_riscv64gc.so"
+    ;;
+  *)
+    echo "Unknown architecture"
+    exit 1;
+    ;;
+esac
--- tool/build_windows.sh
+++ tool/build_windows.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "x64" ]; then
-  cargo build -Z build-std=panic_abort,core,alloc -p powersync_loadable --release --target x86_64-pc-windows-msvc
-  mv "target/x86_64-pc-windows-msvc/release/powersync.dll" "powersync_x64.dll"
-else
-  #Note: aarch64-pc-windows-msvc has not been tested.
-  cargo build -Z build-std=panic_abort,core,alloc -p powersync_loadable --release --target aarch64-pc-windows-msvc
-  mv "target/aarch64-pc-windows-msvc/release/powersync.dll" "powersync_aarch64.dll"
-fi
+case "$1" in
+  x64)
+    cargo build -Z build-std=panic_abort,core,alloc -p powersync_loadable --release --target x86_64-pc-windows-msvc
+    mv "target/x86_64-pc-windows-msvc/release/powersync.dll" "powersync_x64.dll"
+    ;;
+  x86)
+    cargo build -Z build-std=panic_abort,core,alloc -p powersync_loadable --release --target i686-pc-windows-msvc
+    mv "target/i686-pc-windows-msvc/release/powersync.dll" "powersync_x86.dll"
+    ;;
+  aarch64)
+    cargo build -Z build-std=panic_abort,core,alloc -p powersync_loadable --release --target aarch64-pc-windows-msvc
+    mv "target/aarch64-pc-windows-msvc/release/powersync.dll" "powersync_aarch64.dll"
+    ;;
+  *)
+    echo "Unknown architecture"
+    exit 1
+    ;;
+esac
