936d34608829c329d306755b7d38d16f03f1a23c
Simon Binder <simon@journeyapps.com>
2025-08-19 09:10:24 +0200

    Also do that for Linux

--- tool/build_linux.sh
+++ tool/build_linux.sh
@@ -6,6 +6,10 @@ case "$1" in
     cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target x86_64-unknown-linux-gnu
     mv "target/x86_64-unknown-linux-gnu/release/libpowersync.so" "libpowersync_x64.so"
     ;;
+  x86)
+    cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target i686-unknown-linux-gnu
+    mv "target/i686-unknown-linux-gnu/release/libpowersync.so" "libpowersync_x86.so"
+    ;;
   aarch64)
     cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target aarch64-unknown-linux-gnu
     mv "target/aarch64-unknown-linux-gnu/release/libpowersync.so" "libpowersync_aarch64.so"
