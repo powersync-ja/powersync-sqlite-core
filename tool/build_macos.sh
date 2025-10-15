4e00c1b8301f92a5ee062b74f0bb6e42992c7e4a
Simon Binder <simon@journeyapps.com>
2025-10-15 09:47:57 +0200

    Build iOS libraries as dylibs too

--- tool/build_macos.sh
+++ tool/build_macos.sh
@@ -4,16 +4,27 @@ set -e
 function compile() {
   local triple=$1
   local suffix=$2
+  local os=$3
 
   cargo build -p powersync_loadable -Z build-std=panic_abort,core,alloc --release --target $triple
   cargo build -p powersync_static -Z build-std=panic_abort,core,alloc --release --target $triple
 
-  mv "target/$triple/release/libpowersync.dylib" "libpowersync_$suffix.dylib"
-  mv "target/$triple/release/libpowersync.a" "libpowersync_$suffix.macos.a"
+  mv "target/$triple/release/libpowersync.dylib" "libpowersync_$suffix.$os.dylib"
+  mv "target/$triple/release/libpowersync.a" "libpowersync_$suffix.$os.a"
 }
 
-if [ "$1" = "x64" ]; then
-  compile x86_64-apple-darwin x64
-else
-  compile aarch64-apple-darwin aarch64
-fi
+case "$1" in
+  x64)
+    compile x86_64-apple-darwin x64 macos
+    compile x86_64-apple-ios x64 ios-sim
+    ;;
+  aarch64)
+    compile aarch64-apple-darwin aarch64 macos
+    compile aarch64-apple-ios-sim aarch64 ios-sim
+    compile aarch64-apple-ios aarch64 ios
+    ;;
+  *)
+    echo "Unknown architecture"
+    exit 1;
+    ;;
+esac
