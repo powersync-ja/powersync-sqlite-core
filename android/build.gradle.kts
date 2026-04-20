0b41fb4bb9137d918b8c73ad8baec741661b7493
Simon Binder <simon@journeyapps.com>
2026-04-20 11:31:07 +0200

    Prepare 0.4.13 release

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.powersync"
-version = "0.4.12"
+version = "0.4.13"
 description = "PowerSync Core SQLite Extension"
 
 val localRepo = uri("build/repository/")
--- tool/build_xcframework.sh
+++ tool/build_xcframework.sh
@@ -29,7 +29,7 @@ TARGETS=(
   aarch64-apple-tvos-sim
   x86_64-apple-tvos
 )
-VERSION=0.4.12
+VERSION=0.4.13
 
 function generatePlist() {
   min_os_version=0
