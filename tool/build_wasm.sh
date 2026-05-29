b645cdf23d8c7caf12b5cb9fbda47cd127b52871
Simon Binder <simon@journeyapps.com>
2026-05-29 11:18:55 +0200

    Remove unused wasm builds

--- tool/build_wasm.sh
+++ tool/build_wasm.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 set -e
-emcc --version
-
-# Normal build
-# target/wasm32-unknown-emscripten/wasm/powersync.wasm
-RUSTFLAGS="-C link-arg=-sSIDE_MODULE=2" \
-  cargo build \
-    -p powersync_loadable \
-    --profile wasm \
-    --no-default-features \
-    --features "static nightly" \
-    -Z build-std=panic_abort,core,alloc \
-    --target wasm32-unknown-emscripten
-
-cp "target/wasm32-unknown-emscripten/wasm/powersync.wasm" "libpowersync.wasm"
-
-# Asyncify
-# target/wasm32-unknown-emscripten/wasm_asyncify/powersync.wasm
-RUSTFLAGS="-C link-arg=-sSIDE_MODULE=2 -C link-arg=-sASYNCIFY=1 -C link-arg=-sJSPI_IMPORTS=@wasm/asyncify_imports.json" \
-  cargo build \
-    -p powersync_loadable \
-    --profile wasm_asyncify \
-    --no-default-features \
-    --features "static nightly" \
-    -Z build-std=panic_abort,core,alloc \
-    --target wasm32-unknown-emscripten
-
-cp "target/wasm32-unknown-emscripten/wasm_asyncify/powersync.wasm" "libpowersync-async.wasm"
-
 
 # Static lib.
 # Works for both sync and asyncify builds.
