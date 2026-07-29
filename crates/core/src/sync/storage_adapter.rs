f5d43823c87c8cdbcf8713be94baae1fa58b854c
Simon Binder <simon@journeyapps.com>
2026-07-29 17:33:39 +0200

    typo

--- crates/core/src/sync/storage_adapter.rs
+++ crates/core/src/sync/storage_adapter.rs
@@ -51,7 +51,7 @@ pub struct StorageAdapter {
 
 impl StorageAdapter {
     pub fn new(db: *mut sqlite::sqlite3) -> Result<Self, PowerSyncError> {
-        // The cached statements here prevent sqlite3_close to complete. sqlite3_close invokes
+        // The cached statements here prevent sqlite3_close from completing. sqlite3_close invokes
         // the xDisconnect callback on attached virtual tables, which we use to implement a
         // "pre-close hook". See `pre_close_vtab.rs` for more details on how that works.
         ensure_has_internal_close_vtab(db)?;
